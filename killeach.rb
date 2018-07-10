3cdc1a73708489c02e5a29de5d3f3df4ae27e528
Cj-bc <cj-bc@y-modify.org>
2018-07-10 13:09:55 +0900

    Ver: 0.2.0

--- killeach.rb
+++ killeach.rb
@@ -1,13 +1,14 @@
 class Killeach < Formula
   desc "Kill all processes provided from stdin."
   homepage "https://github.com/Cj-bc/killeach"
-  url "https://github.com/Cj-bc/killeach/release/v0.1.0.tar.gz"
-  version "v0.1.0"
-  sha256 "3d7802b86cf83bc71054a4ae7d10c167c3811c5536c328cc88202776c5525bcd"
+  head "https://github.com/Cj-bc/killeach.git"
+  url "https://github.com/Cj-bc/killeach/archive/v0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "8d0de7a6dd823309cd885cee1fd7bbdd81790292b820c56076e14ee3febe7460"
+
 
   def install
     system "mv", "killeach.sh", "killeach"
     bin.install "killeach"
   end
-
 end
